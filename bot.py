db4180b228a21c12b6079027857a3732275e99a3
Danis Art <nikita.efremov1412@gmail.com>
2024-02-05 22:21:03 +0300

    Добавил файл с сохраненными играми

---
+++ bot.py
@@ -0,0 +1,12 @@
+import random as r, test
+""" Запускает легкого бота """
+def easy_bot() -> int:
+    return r.choice(list(set(range(9)) - set(test.moves_made)))
+
+""" Запускает нормального бота """
+def normal_bot():
+    pass
+    
+""" Запускает сложного бота """
+def hard_bot():
+    pass
