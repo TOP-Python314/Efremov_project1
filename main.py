35c2216a872ad915107422020bcdcd4bc3baad02
Danis Art <nikita.efremov1412@gmail.com>
2024-02-05 22:13:12 +0300

    Переписал переменные, Добавил текст к началу игры

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 """ Первый проект """
 
-import func, draw
+import func, draw, test
 
-print('Приветствую тебя в игре "Крестики и Нолики"')
-name = input('Введите свое имя: ')
-print(f'{name}, желаете начать игру?')
+print(' Приветствую тебя в игре "Крестики и Нолики" ')
+name = input(' Введите свое имя: ')
+player_stats = func.finds_name(name) # Статистика игрока
+# Добавить проверку на не оконченные игры
+start = input(f' {name}, желаете начать игру? да/нет: ').lower()
 
-s = input('y/n -> ')
-if s == 'y':    
+if start == 'да':
     func.start_play(name)
 else:
-    print(f'До скорой встречи, {name}!')
+    print(f' До скорой встречи, {name}! ')
     
         
         
