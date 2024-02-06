9dfec4f62e8ec5143ce47e4cc74e1f065514515e
Danis Art <nikita.efremov1412@gmail.com>
2024-02-06 18:40:02 +0300

    Добавил функцию wins

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-def func():
-    pass
-
 wins = [
     {0, 1, 2},
     {3, 4, 5},
@@ -11,19 +8,44 @@ wins = [
     {0, 4, 8},
     {2, 4, 6}
 ]
-moves_made = []
+""" Создает список выйгрышных комбинаций """
+def wins(cells: int) -> list:
+    wins = []
+    cells = 5
+    for i in range(cells):
+        wins.append({*range(i*cells, (i+1)*cells)})
+        wins.append({*range(i, cells**2, cells)})
+    wins.append({*range(0, cells**2, cells+1)})
+    wins.append({*range(cells-1, cells**2-1, cells-1)})
+    return wins
 
-def win_test(name):
-    return any(set(name)>=win for win in wins)
-    
+moves_made = []   
+""" Проверяет наличие выйгрышной комбинации """
+def win_test(names, turn: int) -> str | list:
+    moves1 = [moves_made[i] for i in range(0, len(moves_made), 2)] # ходы для крестика   
+    moves2 = [moves_made[i] for i in range(1, len(moves_made), 2)] # ходы для нолика
+    vars_moves1 = list(filter(lambda x: len(x & set(moves2)) == 0, wins)) # список оставшихся возможных комбинаций для крестика
+    vars_moves2 = list(filter(lambda x: len(x & set(moves1)) == 0, wins)) # для нолика
+    if len(vars_moves1) == 0 and len(vars_moves2) == 0:
+        game_end = input(f' Возможные комбинации побед закончились. Партия будет закончена в ничью. Хотите продолжить игру? да/нет ')
+        if game_end == 'да':
+            return names   
+    elif any(set(moves1)>=win for win in vars_moves1):
+        return names[turn]
+        # победа крестика
+    elif any(set(moves2)>=win for win in vars_moves2):
+        return names[turn-1]
+        # победа нолика
+
 """ Проверяет занята ячейка или нет """
-def step_test():
-    step = int(input())
+def step_test() -> int:
+    step = int(input('Введите номер клетки: '))
     while True:
         if step in moves_made:
             step = int(input('Данная клетка уже занята\n'))
         else:
             return step
-    
-    
+            
+
+   
     
